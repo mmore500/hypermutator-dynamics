1216eaa951da842c491d29e7447c4c0b737cc127
mmore500 <mmore500.login@gmail.com>
2024-10-25 16:45:25 +0000

    Deploying to binder-wse-denovo-spatial2d-explicitsite-traits.ipynb from @ mmore500/hypermutator-dynamics@566f644a1e7dc56395303c3eb6bb3d15f46434ec 🚀

--- subinit_shallow.sh
+++ subinit_shallow.sh
@@ -6,4 +6,4 @@ git submodule update --init --single-branch  # full clone for tex submodule
 git submodule update --init --filter=blob:none --recursive --single-branch --jobs $(nproc)
 
 # setup branch tracking
-git submodule foreach --recursive 'git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"; git checkout -b $(basename $PWD);  git fetch origin $(basename $PWD) --filter=blob:none; git branch -u origin/$(basename $PWD); git config branch.$(basename $PWD).remote origin'
+git submodule foreach --recursive 'git config remote.origin.fetch "+refs/heads/$(basename $PWD):refs/remotes/origin/$(basename $PWD)"; git checkout -b $(basename $PWD); git fetch origin $(basename $PWD) --filter=blob:none; git branch -u origin/$(basename $PWD); git config branch.$(basename $PWD).remote origin'
