40d58d087c39a815bf85353815aa90484f281362
mmore500 <mmore500.login@gmail.com>
2024-10-25 15:02:08 +0000

    Deploying to binder-cupy-5050-spatial2d-genomes.ipynb from @ mmore500/hypermutator-dynamics@d93d42e999d2afef1755243a3da39a9a0af909ad 🚀

--- subinit_shallow.sh
+++ subinit_shallow.sh
@@ -2,6 +2,6 @@
 
 git submodule update --init --single-branch  # full clone for tex submodule
 # blobless clone binder submodules inside tex
-(cd tex && git pull origin tex)
+(cd tex && git checkout -b tex && git pull origin tex)
 # see https://github.blog/open-source/git/get-up-to-speed-with-partial-clone-and-shallow-clone/
 git submodule update --init --filter=blob:none --recursive --single-branch --jobs $(nproc)
