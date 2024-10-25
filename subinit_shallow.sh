ac66d03e0556210b2d69cfcb94d72457a4c9ce5d
mmore500 <mmore500.login@gmail.com>
2024-10-25 16:19:01 +0000

    Deploying to binder-perf-wse-vs-gpu.ipynb from @ mmore500/hypermutator-dynamics@6cb5bb7e744624bb59914530348656da97409629 🚀

--- subinit_shallow.sh
+++ subinit_shallow.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 git submodule update --init --single-branch  # full clone for tex submodule
-# blobless clone binder submodules inside tex
-(cd tex && git checkout -b tex && git pull origin tex)
+
 # see https://github.blog/open-source/git/get-up-to-speed-with-partial-clone-and-shallow-clone/
 git submodule update --init --filter=blob:none --recursive --single-branch --jobs $(nproc)
 
