764caebcf10d6021570d6aa7da5249378d8e8689
mmore500 <mmore500.login@gmail.com>
2024-10-25 16:18:48 +0000

    Deploying to binder-cupy-denovo-wellmixed-traits.ipynb from @ mmore500/hypermutator-dynamics@665f9083e445530d61b18359517cfb9f81a667e8 🚀

--- subinit_shallow.sh
+++ subinit_shallow.sh
@@ -5,3 +5,6 @@ git submodule update --init --single-branch  # full clone for tex submodule
 (cd tex && git checkout -b tex && git pull origin tex)
 # see https://github.blog/open-source/git/get-up-to-speed-with-partial-clone-and-shallow-clone/
 git submodule update --init --filter=blob:none --recursive --single-branch --jobs $(nproc)
+
+# setup branch tracking
+git submodule foreach --recursive 'git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"; git checkout -b $(basename $PWD); git branch -u origin/$(basename $PWD); git fetch origin $(basename $PWD) --filter=blob:none; git config branch.$(basename $PWD).remote origin'
