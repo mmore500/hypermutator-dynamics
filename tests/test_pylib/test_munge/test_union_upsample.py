b6ae9c82095f97a87a8ed9c7f05a2b44d41befc4
mmore500 <mmore500.login@gmail.com>
2025-01-13 06:58:31 +0000

    Deploying to binder-wse-5050-spatial2d-32atile-infben-traits.ipynb from @ mmore500/hypermutator-dynamics@59ae0b626c68636c1bc6de927897a113c3189e86 🚀

---
+++ tests/test_pylib/test_munge/test_union_upsample.py
@@ -0,0 +1,71 @@
+from pylib.munge._union_upsample import union_upsample
+
+
+import polars as pl
+from polars.testing import assert_frame_equal
+
+
+def test_upsample_union_col():
+    df = pl.DataFrame(
+        {
+            "group": ["A", "A", "B"],
+            "timestamp": [1, 2, 1],
+            "value": [10, 1, 5],
+            "value2": [10, 1, 5],
+        }
+    )
+
+    result = union_upsample(
+        df,
+        upsample="timestamp",
+        group_by="group",
+        fill_null_ops=[(pl.col("value"), {"strategy": "forward"})],
+    )
+
+    expected = pl.DataFrame(
+        {
+            "group": ["A", "A", "B", "B"],
+            "timestamp": [1, 2, 1, 2],
+            "value": [10, 1, 5, 5],
+            "value2": [10, 1, 5, None],
+        }
+    )
+
+    assert_frame_equal(
+        result,
+        expected,
+        check_column_order=False,
+        check_row_order=False,
+    )
+
+
+def test_upsample_union_all():
+    df = pl.DataFrame(
+        {
+            "group": ["A", "A", "B"],
+            "timestamp": [1, 2, 1],
+            "value": [10, 1, 5],
+        }
+    )
+
+    result = union_upsample(
+        df,
+        upsample="timestamp",
+        group_by="group",
+        fill_null_ops=[(pl.all(), {"strategy": "forward"})],
+    )
+
+    expected = pl.DataFrame(
+        {
+            "group": ["A", "A", "B", "B"],
+            "timestamp": [1, 2, 1, 2],
+            "value": [10, 1, 5, 5],
+        }
+    )
+
+    assert_frame_equal(
+        result,
+        expected,
+        check_column_order=False,
+        check_row_order=False,
+    )
